402f1b1f41546c0d767ad1934fbf4721c8a939d3
codexpotech <codexpowebs@gmail.com>
2019-02-12 01:37:42 +0530

    Minor change on css imports

--- src/components/App.vue
+++ src/components/App.vue
@@ -20,7 +20,6 @@ export default {
 
 <style>
   @import '../static/css/main.css';
-  @import '../static/vendor/fontawesome-free/css/all.min.css';
   @import "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css";
   @import "https://cdn.jsdelivr.net/npm/bootstrap-vue@2.0.0-rc.11/dist/bootstrap-vue.css";
 
