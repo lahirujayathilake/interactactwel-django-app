484f58796502f61b6a9652e8b33859f15529588e
asankah <asankah@zone24x7.com>
2019-05-23 22:45:15 +0530

    adding marker icon popups

---
+++ interactwel/dashboard/src/components/dashboard/map/popup/PopupContent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        This is the  {{ data.name }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PopupContent",
+        props: {
+            data: {
+                name: String
+            }
+        }
+    };
+</script>
