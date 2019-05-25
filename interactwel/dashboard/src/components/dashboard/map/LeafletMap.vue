9475d3dcab6a9ecfd8f13fb2da34e5b2a7fd28c5
asankah <asankah@zone24x7.com>
2019-05-25 19:36:16 +0530

    weather station popup graph - showing data related to one weather station only

--- interactwel/dashboard/src/components/dashboard/map/LeafletMap.vue
+++ interactwel/dashboard/src/components/dashboard/map/LeafletMap.vue
@@ -184,7 +184,7 @@
 
                 weatherStationList: [
                     {id: "1", name:'USC00353827 (Hempner, OR)', position: {lat: 45.365, lng: -119.584}, data_Range: '1/1/1940 – 12/31/2010', obs_type: 'Daily', num_Pobs: '25,840', num_Tobs: '25,832'},
-                    {id: "2", name: 'USC00354161  (Ione 18 S, OR US)', position: {lat: 45.317, lng: -119.881}, data_Range: '1/1/1940 – 5/31/2010', obs_type: 'Daily', num_Pobs: '23,208', num_Tobs: '0'}
+                    {id: "2", name: 'USC00354161 (Ione 18 S, OR US)', position: {lat: 45.317, lng: -119.881}, data_Range: '1/1/1940 – 5/31/2010', obs_type: 'Daily', num_Pobs: '23,208', num_Tobs: '0'}
                 ],
 
                 reservoirStationList: [
