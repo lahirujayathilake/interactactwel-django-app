cd94d5da08f98bd40ebcf3be350c0c2a1229cb45
codexpotech <codexpowebs@gmail.com>
2019-05-27 17:57:11 +0530

    Update feedback form content

--- interactwel/dashboard/src/components/dashboard/feedback/feedback.vue
+++ interactwel/dashboard/src/components/dashboard/feedback/feedback.vue
@@ -1,104 +1,92 @@
 <template>
     <div class="feedback-container">
         <div class="header-container">
-            <!-- <h6>POPSWAT_0</h6> -->
-            <h4>Evaluate Adaptation Plan</h4>
+            <h5>Evaluate Adaptation Plan</h5>
         </div>
-        <div id="feedback-step1" class="ui form">
-            <div class="field">
-                <label>Do you think this Alternative plan is feasible?</label>
-                <div class="inline fields">
-                    <div class="field">
-                        <div class="ui radio checkbox">
-                            <input class="" type="radio" name="feasibility" id="feasibility-yes">
-                            <label>Yes</label>
-                        </div>
-                        <div class="ui radio checkbox">
-                            <input class="" type="radio" name="feasibility" id="feasibility-no">
-                            <label>No</label>
-                        </div>
-                        <div class="ui radio checkbox">
-                            <input class="" type="radio" name="feasibility" id="feasibility-maybe">
-                            <label>May be</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div hidden id="no-feasible" class="grouped fields">
-                <label>why is this plan not feasible? (Check all that apply)</label>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="top-posts">
-                        <label>Unlikely to be profitable / financially sustainable</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Infrastructure costs</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Permits or other regulatory approval processes and cost</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Reliance on other stakeholders to take action</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Long time period before seeing positive results</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Public disapproval of the actions listed in the plans</label>
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="hot-deals">
-                        <label>Other</label>
-                    </div>
-                </div>
-                <div class="field wide">
-                    <label>Short Text</label>
-                    <textarea placeholder="Feel free to add additional comments about your rating here:"
-                              rows="7"></textarea>
-                </div>
-            </div>
-            <b-button variant="success">Submit</b-button>
+        <div id="feedback-block" class="ui form">
+            <b-form @submit="onSubmit" @reset="onReset">
+                <b-form-group label="Do you think this Alternative plan is feasible?">
+                    <b-form-radio v-model="checked" name="some-radios" value="A">Yes</b-form-radio>
+                    <b-form-radio v-model="checked" name="some-radios" value="B">No</b-form-radio>
+                    <b-form-radio v-model="checked" name="some-radios" value="C">May Be</b-form-radio>
+                </b-form-group>
+                <b-form-group id="input-group-4" label="Why is this plan not feasible? (Check all that apply)">
+                    <b-form-checkbox v-model="selected" value="1">Unlikely to be profitable / financially sustainable</b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="2">Infrastructure costs</b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="3">Permits or other regulatory approval processes and cost
+                    </b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="4">Reliance on other stakeholders to take action</b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="5">Long time period before seeing positive results</b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="6">Public disapproval of the actions listed in the plans
+                    </b-form-checkbox>
+                    <b-form-checkbox v-model="selected" value="7">Other</b-form-checkbox>
+                </b-form-group>
+                <b-form-textarea
+                        id="textarea"
+                        v-model="comment"
+                        placeholder="Provide your feedback"
+                        rows="3"
+                        max-rows="6"
+                ></b-form-textarea>
+                <b-button id="submit-btn" type="submit" variant="success">Submit</b-button>
+            </b-form>
         </div>
-
     </div>
 </template>
 
 <script>
 
     export default {
-        name: 'Feedback'
+        name: 'Feedback',
+
+        data() {
+            return {
+            }
+        },
+        methods: {
+            onSubmit(evt) {
+                evt.preventDefault()
+                alert(JSON.stringify(this.form))
+            },
+            onReset(evt) {
+                evt.preventDefault()
+                // Reset our form values
+                this.form.comment = null
+                this.form.checked = []
+                this.form.selected = []
+                // Trick to reset/clear native browser form validation state
+                this.show = false
+                this.$nextTick(() => {
+                    this.show = true
+                })
+            }
+        }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
-    #feedback-step1 {
+    #feedback-block {
         background-color: #f5f8fa;
         padding: 20px;
         border-radius: 10px;
         margin-top: 20px;
+        font-size: 12px;
+
+    }
+
+    #feedback-block legend{
+        font-weight: bold;
+        font-size: 14px;
     }
 
     .header-container {
         padding: 0 10px;
+    }
+
+    #submit-btn{
+        margin-top:10px;
     }
 
 </style>
