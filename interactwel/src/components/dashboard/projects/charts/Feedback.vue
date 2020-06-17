6e6e53c81ab314b76c016652aa9f48fcf4c2b38e
codexpotech <codexpowebs@gmail.com>
2020-06-17 21:51:55 +0530

    fixing the project fetch issue caused after the API change

--- interactwel/src/components/dashboard/projects/charts/Feedback.vue
+++ interactwel/src/components/dashboard/projects/charts/Feedback.vue
@@ -11,8 +11,7 @@
                             <b-form-radio v-model="feasibility" value="1" name="some-radios">Yes</b-form-radio>
                             <b-form-radio v-model="feasibility" value="0" name="some-radios">No</b-form-radio>
                         </b-form-group>
-                    </b-form>
-                    <b-form v-if="feasibility === '0'">
+                    <div v-if="feasibility === '0'">
                         <b-form-group label="Please state why this plan is infeasible">
                             <b-form-textarea
                                     id="textarea"
@@ -21,82 +20,28 @@
                                     max-rows="6"
                             ></b-form-textarea>
                         </b-form-group>
-                    </b-form>
-                    <b-form v-if="feasibility === '1'">
+                    </div>
+                    <div v-if="feasibility === '1'">
 
-                        <b-form-group
-                                label="How well does this plan (alternative plan 1, 2, etc.) meet the management goals you identified in Step 1?">
-                            <b-form-radio-group
-                                    id="radio-group-1"
-                                    v-model="q1Selected"
+                        <b-form-group v-for="question in questions" :key="question.question_id"
+                                :label="question.question_text">
+                            <b-form-radio-group v-if="question.question_type === 'multi_choice'"
+                                    :id="question.question_id"
+                                    v-model="form.answers[question.question_id]"
                                     :options="options"
-                                    name="q1"
                             ></b-form-radio-group>
-                        </b-form-group>
 
-                        <b-form-group
-                                label="Do you think that the projected results/impacts from this plan will occur if the proposed actions are implemented?">
-                            <b-form-radio-group
-                                    id="radio-group-2"
-                                    v-model="q2Selected"
-                                    :options="options"
-                                    name="q2"
-                            ></b-form-radio-group>
-                        </b-form-group>
-
-                        <b-form-group label="What factors might prevent you from taking the recommended actions listed in this plan? (check all that apply)">
-                            <b-form-checkbox-group
-                                    id="checkbox-group-1"
-                                    v-model="SelectedFactors"
-                                    :options="factors"
-                                    name="q3"
-                            ></b-form-checkbox-group>
-                        </b-form-group>
-
-                        <b-form-group
-                                label="What factors might prevent you from taking the recommended actions listed in this plan? (check all that apply)">
-                            <b-form-radio-group
-                                    id="radio-group-3"
-                                    v-model="q3Selected"
-                                    :options="options"
-                                    name="q4"
-                            ></b-form-radio-group>
-                        </b-form-group>
-
-                        <b-form-group
-                                label="How likely are you to implement the actions recommended in this plan?">
-                            <b-form-radio-group
-                                    id="radio-group-4"
-                                    v-model="q4Selected"
-                                    :options="options"
-                                    name="q5"
-                            ></b-form-radio-group>
-                        </b-form-group>
-
-                        <b-form-group
-                                label="How likely are you to work with other stakeholder groups to implement this plan?">
-                            <b-form-radio-group
-                                    id="radio-group-5"
-                                    v-model="q5Selected"
-                                    :options="options"
-                                    name="q6"
-                            ></b-form-radio-group>
-                        </b-form-group>
-                        <b-form-group label="Rate this plan">
-                            <star-rating star-size="30  "></star-rating>
-                        </b-form-group>
-
-                        <b-form-group label="Please share any additional thoughts about the alternative plans or the plan builder tool">
-                            <b-form-textarea
-                                    id="textarea"
-                                    v-model="ifNotFeasiblecomment"
-                                    rows="3"
-                                    max-rows="6"
+                            <b-form-textarea v-if="question.question_type === 'text'"
+                                             :id="question.question_id"
+                                             v-model="form.answers[question.question_id]"
+                                             rows="3"
+                                             max-rows="6"
                             ></b-form-textarea>
-                        </b-form-group>
 
+                        </b-form-group>
+                    </div>
+                    <b-button type="submit" @click="submitFeedback">Submit</b-button>
                     </b-form>
-                    <b-button @click="submitFeedback">Submit</b-button>
                 </div>
                 <div id="thankyou-block" v-show="thankyouBlock">
                     Thank you
@@ -149,15 +94,32 @@
                 SelectedFactors: [],
 
                 questions: [],
+                form: {
+                    answers: []
+                }
             }
         },
         mounted() {
+            const {utils} = AiravataAPI;
+
             if(!localStorage.feedbackProvided){
                 localStorage.setItem('feedbackProvided', false);
             }else{
                 let $this = this
                 $this.feedbackProvided = localStorage.feedbackProvided;
             }
+
+            utils.FetchUtils.get("/interactwel/api/questions/")
+                .then(questions => {
+                    console.log(questions);
+                    if (questions != null) {
+                        this.questions = questions.sort((x, y) => x.question_id - y.question_id)
+                    }
+                })
+                .catch(error => {
+                    alert("Could not get the projects list. API error! " + error);
+                });
+
 
         },
         watch: {},
@@ -171,6 +133,8 @@
                 this.$router.push('/adaptation-plans/1/actions')
             },
             submitFeedback() {
+                debugger;
+                console.log(this.form)
                 let adaptationPlan = JSON.parse(localStorage.getItem("adaptationPlan"));
                 adaptationPlan.planId = this.$route.params.planId;
                 localStorage.setItem('adaptationPlan', JSON.stringify(adaptationPlan));
