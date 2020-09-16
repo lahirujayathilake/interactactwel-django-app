f62de3797fbd99e27a71a0956df9bb70e68ed045
SammyRivera <sammy.rivera14@gmail.com>
2020-09-16 10:32:45 -0700

    Update ActionsGraphSteppedLines.vue

--- interactwel/src/components/dashboard/projects/charts/data/ActionsGraphSteppedLines.vue
+++ interactwel/src/components/dashboard/projects/charts/data/ActionsGraphSteppedLines.vue
@@ -32,8 +32,8 @@
                         borderColor: "#186a3b",
                         borderJoinStyle: 'round',
                         borderWidth: 6,
-                        data: cr_data,
-                        //data: ['Increase amount of SW','Increase amount of SW', 'Increase amount of SW', 'Increase amount of SW','Increase amount of CR','Increase amount of CR','Increase amount of CR','Business as usual','Business as usual','Business as usual'],
+                        data: [
+                                'Increase amount of SW','Increase amount of SW', 'Increase amount of SW', 'Increase amount of SW','Increase amount of CR','Increase amount of CR','Increase amount of CR','Business as usual','Business as usual','Business as usual'],
                         steppedLine: true,
                         fill: false,
                     },
