fecd9b731b90d242b0d0ed0a920c1ccddb59bded
SammyRivera <sammy.rivera14@gmail.com>
2020-09-15 23:37:06 -0700

    Update ActionsGraphSteppedLines.vue

--- interactwel/src/components/dashboard/projects/charts/data/ActionsGraphSteppedLines.vue
+++ interactwel/src/components/dashboard/projects/charts/data/ActionsGraphSteppedLines.vue
@@ -93,6 +93,45 @@
                     }
                 },
             }
+        },
+        mounted() {
+            let $this = this;
+            EventBus.$on('CLICK_ITEM_SIDEBAR', function (planName) {
+                $this.planName = planName;
+                $this.buildDataCollection($this.JSONData, $this.planName);
+            });
+
+        },
+
+        created(){
+            axios.get("/static/BASIN_Water_Rights_plans.json").then(response => {
+                this.JSONData = response.data;
+                this.buildDataCollection(this.JSONData, this.planName);
+            });
+        },
+
+        methods: {
+            buildDataCollection(data, adaptationPlan){
+                this.datacollection = {};
+                this.datacollection.labels = [];
+                for (let legend in data.Legend) {
+                    this.datacollection.labels.push(data.Legend[legend]);
+                }
+                this.datacollection.datasets = [];
+                let i= 0;
+                for (let dataIndex in data.Adaptation_plans[adaptationPlan]["Data"]) {
+                    let dataPoint = data.Adaptation_plans[adaptationPlan]["Data"][dataIndex];
+                    let dataset = {};
+                    dataset.label = dataPoint.Name;
+                    dataset.backgroundColor = this.getColor(i++);
+                    dataset.data = [];
+                    for (let dataValue in dataPoint.Data) {
+                        dataset.data.push(dataPoint.Data[dataValue]);
+                    }
+                    this.datacollection.datasets.push(dataset);
+                }
+                i++;
+            }
         }
-    }
+    };
 </script>
