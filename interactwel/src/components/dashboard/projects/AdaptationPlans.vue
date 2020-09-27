fd52cc225ca0bf29059c43cd22596f5846d5daff
codexpotech <codexpowebs@gmail.com>
2020-09-27 17:46:31 +0530

    Minor change

--- interactwel/src/components/dashboard/projects/AdaptationPlans.vue
+++ interactwel/src/components/dashboard/projects/AdaptationPlans.vue
@@ -4,7 +4,7 @@
             <b-navbar toggleable="sm" type="dark" variant="dark">
                 <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-                <b-navbar-brand>Project Id 76876 : {{ $route.params.projectId }}</b-navbar-brand>
+                <b-navbar-brand>Project Id : {{ $route.params.projectId }}</b-navbar-brand>
 
                 <b-collapse id="nav-text-collapse" is-nav>
                     <b-navbar-nav>
