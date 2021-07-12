72321bac491b3b5d1238bc876a82b3a97d118bc3
codexpotech <codexpowebs@gmail.com>
2021-07-12 21:09:29 +0530

    change button name

--- interactwel/src/components/dashboard/projects/AssignPlans.vue
+++ interactwel/src/components/dashboard/projects/AssignPlans.vue
@@ -24,7 +24,7 @@
   <td>"Improve surface water quality“, "Increase recharge to shallow aquifer“, "Increase agriculture productivity“ , "Improve ecological habitat"</td>
   <td>Increase the volume of non-Columbia surface water“, "Decrease the volume of non-Columbia surface water“</td>
   <td>
-    <div class="btn btn-success btn-sm">Deploy</div>
+    <div class="btn btn-success btn-sm">Assign</div>
   </td>
   <td><div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div></td>
 </tr>
